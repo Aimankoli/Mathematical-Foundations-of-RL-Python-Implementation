d895678131988219b13b056080f8e2a0a0f8de26
Aiman <m.aimankoli@gmail.com>
2025-04-29 14:14:21 -0400

    init

---
+++ src/grid.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Grid:
+    def __init__(self, rows, cols, holes, start, goal):
+        self.rows = rows
+        self.cols = cols
+        self.holes = holes
+        self.start = start
+        self.goal = goal
+    
+    def generate_grid(self):
+        pass
+if __name__ == "__main__":
+    print ("TBD")
+        
